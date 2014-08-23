ebecf4b1431f5c788397d86e093ba7e4b2f33633
Carl Edwards <ecarl42@gmail.com>
2014-08-23 16:15:26 -0700

    the battery value now scrolls in and out slower.   Also added where the date label layer follows the battery level layer.

--- watchface/src/basebar.c
+++ watchface/src/basebar.c
@@ -11,6 +11,7 @@ GBitmap * no_bt_image;
 GFont s_digital_font;
 
 PropertyAnimation *batt_animation = NULL;
+PropertyAnimation *date_animation = NULL;
 
 void basebar_setup(Layer *window_layer) {
   // load our custom font for the date/battery 
@@ -19,24 +20,24 @@ void basebar_setup(Layer *window_layer) {
   no_bt_image = gbitmap_create_with_resource(RESOURCE_ID_NO_BT);
 	
   //clip layer
-  clip_layer = layer_create(GRect(0, 134, 144, 34));
+  clip_layer = layer_create(GRect(0, 140, 144, 34));
   layer_add_child(window_layer, clip_layer);
 
   // date label
-  date_label = text_layer_create(GRect(0, 0, 144, 34));
+  date_label = text_layer_create(GRect(0, -40, 144, 34));
   text_layer_set_text_color(date_label, GColorBlack);
   text_layer_set_font(date_label, s_digital_font);
   text_layer_set_text_alignment(date_label, GTextAlignmentCenter);
   layer_add_child(clip_layer, text_layer_get_layer(date_label));
 
-  // no bluetooth icon
-  no_bt_image_layer = bitmap_layer_create(GRect(18, 7, 21, 26));
+  // no BT connection icon
+  no_bt_image_layer = bitmap_layer_create(GRect(18, 1, 21, 26));
   bitmap_layer_set_bitmap(no_bt_image_layer, no_bt_image);
   layer_add_child(clip_layer, bitmap_layer_get_layer(no_bt_image_layer));
   layer_set_hidden(bitmap_layer_get_layer(no_bt_image_layer), true);  
   
   // battery label
-  battery_label = text_layer_create(GRect(0, 0, 144, 34));
+  battery_label = text_layer_create(GRect(0, -6, 144, 34));
   text_layer_set_text_color(battery_label, GColorBlack);
   text_layer_set_font(battery_label, s_digital_font);
   text_layer_set_text_alignment(battery_label, GTextAlignmentCenter);
@@ -49,6 +50,17 @@ void basebar_setup(Layer *window_layer) {
   text_layer_set_text(battery_label, battery_label_buffer);  
 }
 
+void destroy_animations(void) {
+  if (batt_animation != NULL) {
+    property_animation_destroy(batt_animation);
+    batt_animation = NULL;
+  }
+  if (date_animation != NULL) {
+    property_animation_destroy(date_animation);
+    date_animation = NULL;
+  }
+}
+
 void basebar_teardown() {
   text_layer_destroy(date_label);
   text_layer_destroy(battery_label);
@@ -56,10 +68,7 @@ void basebar_teardown() {
   fonts_unload_custom_font(s_digital_font);
   bitmap_layer_destroy(no_bt_image_layer);
   gbitmap_destroy(no_bt_image);
-  if (batt_animation != NULL) {
-    property_animation_destroy(batt_animation);
-    batt_animation = NULL;
-  }
+  destroy_animations();
 }
 
 void basebar_set_date_text(char *time_buffer) {
@@ -75,24 +84,29 @@ void basebar_show_no_bt_image() {
 }
 
 void basebar_hide_battery() {
-  if (batt_animation != NULL) {
-    property_animation_destroy(batt_animation);
-    batt_animation = NULL;
-  }
-  GRect to_frame = GRect(0, 34, 144, 34);
-  batt_animation = property_animation_create_layer_frame(text_layer_get_layer(battery_label), NULL, &to_frame);
+  destroy_animations();
+  GRect battery_label_to_frame = GRect(0, 28, 144, 34);
+  batt_animation = property_animation_create_layer_frame(text_layer_get_layer(battery_label), NULL, &battery_label_to_frame);
   animation_set_curve(&(batt_animation->animation), AnimationCurveLinear);
+  animation_set_duration((Animation *)batt_animation, 700);
   animation_schedule(&(batt_animation->animation));
+  GRect date_label_to_frame = GRect(0, -6, 144, 34);
+  date_animation = property_animation_create_layer_frame(text_layer_get_layer(date_label), NULL, &date_label_to_frame);
+  animation_set_curve(&(date_animation->animation), AnimationCurveLinear);
+  animation_set_duration((Animation *)date_animation, 700);
+  animation_schedule(&(date_animation->animation));
 }
 
 void basebar_show_battery() {
-
-  if (batt_animation != NULL) {
-    property_animation_destroy(batt_animation);
-    batt_animation = NULL;
-  }
-  GRect to_frame = GRect(0, 0, 144, 34);
-  batt_animation = property_animation_create_layer_frame(text_layer_get_layer(battery_label), NULL, &to_frame);
+  destroy_animations();
+  GRect battery_label_to_frame = GRect(0, -6, 144, 34);
+  batt_animation = property_animation_create_layer_frame(text_layer_get_layer(battery_label), NULL, &battery_label_to_frame);
   animation_set_curve(&(batt_animation->animation), AnimationCurveLinear);
+  animation_set_duration((Animation *)batt_animation, 700);
   animation_schedule(&(batt_animation->animation));
+  GRect date_label_to_frame = GRect(0, -40, 144, 34);
+  date_animation = property_animation_create_layer_frame(text_layer_get_layer(date_label), NULL, &date_label_to_frame);
+  animation_set_curve(&(date_animation->animation), AnimationCurveLinear);
+  animation_set_duration((Animation *)date_animation, 700);
+  animation_schedule(&(date_animation->animation));
 }
